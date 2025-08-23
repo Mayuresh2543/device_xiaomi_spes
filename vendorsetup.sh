9dff1656427df529d7db822b66715e20907c36d9
mayuresh2543 <mayureshnanal846@gmail.com>
2025-08-23 15:41:43 +0530

    spes: clone everything with depth=1 and also clone viper

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,14 +14,18 @@ rm -rf hardware/lineage/compat/Android.bp
 
 # Clone kernel/vendortree/xiaomi-hardware
 echo -e "${color}Setup kernel,vendor,xiaomi-hardware tree ${end}"
-git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git -b main kernel/xiaomi/spes
-git clone https://github.com/muralivijay/android_vendor_xiaomi_spes.git -b 13.0 vendor/xiaomi/spes
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git -b main --depth=1 kernel/xiaomi/spes
+git clone https://github.com/muralivijay/android_vendor_xiaomi_spes.git -b 13.0 --depth=1 vendor/xiaomi/spes
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 --depth=1 hardware/xiaomi
 
 # Clone Miuicamera
 echo -e "${color}Setup Miuicamera ${end}"
-git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm6225 vendor/xiaomi/camera/
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm6225 --depth=1 vendor/xiaomi/camera/
 
 # Clone BCR
 echo -e "${color}Setup BCR ${end}"
-git clone https://github.com/Chaitanyakm/vendor_bcr.git vendor/bcr
+git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth=1 vendor/bcr
+
+# Viper
+echo -e "${color}Setup Viper ${end}"
+git clone https://github.com/mayuresh2543/vendor_viper4.git --depth=1 packages/apps/ViPER4AndroidFX
